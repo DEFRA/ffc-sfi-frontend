e8c0f5ab91e798a37acfc78d488f70a812f59dec
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-10-26 14:46:14 +0000

    feat: add hapi modules, webpack and govuk frontend

--- app/server.js
+++ app/server.js
@@ -1,14 +1,47 @@
+const path = require('path')
 const Hapi = require('@hapi/hapi')
-
-const server = Hapi.server({
-  port: process.env.PORT
-})
+const nunjucks = require('nunjucks')
+const vision = require('@hapi/vision')
+const inert = require('@hapi/inert')
 
 const routes = [].concat(
+  require('./routes/assets'),
   require('./routes/healthy'),
-  require('./routes/healthz')
+  require('./routes/healthz'),
+  require('./routes/home')
 )
 
-server.route(routes)
+async function createServer () {
+  const server = Hapi.server({
+    port: process.env.PORT
+  })
 
-module.exports = server
+  await server.register(inert)
+  await server.register(vision)
+
+  server.route(routes)
+
+  server.views({
+    engines: {
+      njk: {
+        compile: (src, options) => {
+          const template = nunjucks.compile(src, options.environment)
+          return context => template.render(context)
+        }
+      }
+    },
+    relativeTo: __dirname,
+    compileOptions: {
+      environment: nunjucks.configure([
+        path.join(__dirname, 'views'),
+        path.join(__dirname, 'assets', 'dist'),
+        'node_modules/govuk-frontend/'
+      ])
+    },
+    path: './views/'
+  })
+
+  return server
+}
+
+module.exports = createServer
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDev = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
+
+console.log(`Running webpack in ${isDev ? 'development' : 'production'} mode`)
+
+module.exports = {
+  entry: './app/frontend/src/entry.js',
+  mode: isDev ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+              publicPath: '../'
+            }
+          },
+          'css-loader',
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDev,
+              sassOptions: {
+                outputStyle: 'compressed'
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'images/'
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'fonts/'
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'js/bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'app/frontend/dist'),
+    publicPath: '/assets/'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      filename: '../../views/layouts/layout.njk',
+      template: 'app/views/layouts/_layout.njk'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/application.[contenthash].css'
+    })
+  ]
+}
