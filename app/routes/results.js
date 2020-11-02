a32ea52db11036f067033d9eb8e9eea3ef7d5c8a
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-11-02 09:38:18 +0000

    feat: add validation

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,7 +1,17 @@
+const Joi = require('@hapi/joi')
+
 module.exports = {
   method: 'POST',
   path: '/results',
   handler: (request, h) => {
     return h.view('results')
+  },
+  options: {
+    validate: {
+      payload: Joi.object({
+        sbi: Joi.string().required()
+      }),
+      failAction: (request, h) => h.view('sbi').takeover()
+    }
   }
 }
