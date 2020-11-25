496dcf4dbad130a6a7364cb364fe77977b687d0f
Paul Andrews <paul.andrews@defra.gov.uk>
2020-11-25 15:26:23 +0000

    Remove queue config duplication and redundant npm packages

--- app/config/messaging.js
+++ app/config/messaging.js
@@ -1,21 +1,19 @@
-const connectionDetails = {
+const sharedConfig = {
   host: process.env.MESSAGE_QUEUE_HOST,
   password: process.env.MESSAGE_QUEUE_PASSWORD,
-  username: process.env.MESSAGE_QUEUE_USER
+  username: process.env.MESSAGE_QUEUE_USER,
+  type: 'queue',
+  usePodIdentity: process.env.NODE_ENV === 'production'
 }
 
 module.exports = {
   updateEligibilityQueue: {
     address: process.env.UPDATE_ELIGIBILITY_QUEUE_ADDRESS,
-    type: 'queue',
-    usePodIdentity: process.env.NODE_ENV === 'production',
-    ...connectionDetails
+    ...sharedConfig
   },
   updateAgreementQueue: {
     address: process.env.UPDATE_AGREEMENT_QUEUE_ADDRESS,
-    type: 'queue',
-    usePodIdentity: process.env.NODE_ENV === 'production',
-    ...connectionDetails
+    ...sharedConfig
   },
   messageTypePrefix: 'uk.gov.ffc.sfi',
   messageSource: 'ffc-sfi-frontend'
