4d51386465df0032a63328ef2bf4b353ba0778cb
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-01 08:51:03 +0000

    feat: install uuid for correlation

--- app/routes/send-message.js
+++ app/routes/send-message.js
@@ -1,3 +1,4 @@
+const { v4: uuid } = require('uuid')
 const { updateAgreement } = require('../messaging/senders')
 
 module.exports = {
@@ -5,10 +6,15 @@ module.exports = {
   path: '/send-message',
   handler: async (request, h) => {
     const { value } = request.payload
-    const msg = { value }
+    const correlationId = uuid()
+    const msg = { correlationId, value }
     console.log('Sending message', msg)
     await updateAgreement(msg)
 
+    // wait for message to be received from agreement update
+    // until that topic exists, hook into the existing
+
+    // return a page that will auto redirect to the page with the id generated
     return h.view('message-sent')
   }
 }
