885cd268d9ec4c52c857c535c191d571b3a2dd4b
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-07 16:47:35 +0000

    feat: make request to agreement microservice

--- app/routes/retrieve-value.js
+++ app/routes/retrieve-value.js
@@ -1,10 +1,13 @@
+const Wreck = require('@hapi/wreck')
+
 module.exports = {
   method: 'GET',
   path: '/retrieve-value',
   handler: async (request, h) => {
     const { correlationId } = request.query
-    console.log(`Retrieving agreement for correlationId: ${correlationId}.`)
-    const data = 'PENDING'
-    return h.view('agreement', { correlationId, data })
+    // TODO: needs to work in cluster
+    const { payload } = await Wreck.get(`http://host.docker.internal:3001/value?correlationId=${correlationId}`, { json: true })
+    console.log(`Retrieving '${JSON.stringify(payload)}' for correlationId: '${correlationId}'.`)
+    return h.view('agreement', { correlationId, payload })
   }
 }
