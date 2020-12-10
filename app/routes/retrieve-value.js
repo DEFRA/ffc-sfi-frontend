a9603a0881f39402477b5d69e0efe23096474c56
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-10 16:58:00 +0000

    Additional logging around request (#17)
    * feat: additional logging around agreement request
    
    * chore: increment version

--- app/routes/retrieve-value.js
+++ app/routes/retrieve-value.js
@@ -6,8 +6,10 @@ module.exports = {
   path: '/retrieve-value',
   handler: async (request, h) => {
     const { correlationId } = request.query
-    const { payload } = await Wreck.get(`${agreementServiceBaseUrl}/value?correlationId=${correlationId}`, { json: true })
-    console.log(`Retrieving '${JSON.stringify(payload)}' for correlationId: '${correlationId}'.`)
+    const url = `${agreementServiceBaseUrl}/value?correlationId=${correlationId}`
+    console.log('Making request to:', url)
+    const { payload } = await Wreck.get(url, { json: true })
+    console.log(`Retrieved '${JSON.stringify(payload)}' for correlationId: '${correlationId}'.`)
     return h.view('agreement', { correlationId, payload })
   }
 }
