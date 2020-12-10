1fb98291c84c5c32a4c7ac0f84fb85a5044120a2
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-10 08:41:20 +0000

    feat: enable endpoint access within cluster

--- app/routes/retrieve-value.js
+++ app/routes/retrieve-value.js
@@ -1,12 +1,12 @@
 const Wreck = require('@hapi/wreck')
+const { agreementServiceBaseUrl } = require('../config/general')
 
 module.exports = {
   method: 'GET',
   path: '/retrieve-value',
   handler: async (request, h) => {
     const { correlationId } = request.query
-    // TODO: needs to work in cluster
-    const { payload } = await Wreck.get(`http://host.docker.internal:3001/value?correlationId=${correlationId}`, { json: true })
+    const { payload } = await Wreck.get(`${agreementServiceBaseUrl}/value?correlationId=${correlationId}`, { json: true })
     console.log(`Retrieving '${JSON.stringify(payload)}' for correlationId: '${correlationId}'.`)
     return h.view('agreement', { correlationId, payload })
   }
