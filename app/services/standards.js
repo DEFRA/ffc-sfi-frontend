b16d8e465731ab8eff838b9b910f6ffefb04a1f1
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-12-22 11:11:30 +0000

    feat: construct entry page based on available standards

---
+++ app/services/standards.js
@@ -0,0 +1,24 @@
+module.exports = {
+  arable: {
+    hint: 'A minimum of 5% of all arable land will need to be set aside. Payment rate is £123 per hectare of total arable land area.',
+    id: 'arable',
+    paymentRate: 123,
+    percentage: 5,
+    text: 'Enter the amount of arable land you have',
+    units: {
+      name: 'hectares',
+      symbol: 'ha'
+    }
+  },
+  grassland: {
+    hint: 'A minimum of 5% of all grassland will need to be set aside. Payment rate is £456 per hectare of total grassland area.',
+    id: 'grassland',
+    paymentRate: 456,
+    percentage: 5,
+    text: 'Enter the amount of grassland you have',
+    units: {
+      name: 'hectares',
+      symbol: 'ha'
+    }
+  }
+}
