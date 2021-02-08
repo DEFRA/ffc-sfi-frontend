73cdce82aafe7de10c88fd70ed9e7542c2d66117
Paul Andrews <paul.andrews@defra.gov.uk>
2021-02-08 16:46:08 +0000

    Display sensible payments

--- app/routes/ready-reckoner-v2/content-funding-summary.js
+++ app/routes/ready-reckoner-v2/content-funding-summary.js
@@ -1,35 +1,39 @@
 function toDisplay (value) {
-  return value?.toLocaleString('en-GB', { maximumFractionDigits: 2 }) ?? 'QQQ'
+  return value?.toLocaleString('en-GB', { maximumFractionDigits: 2 }) ?? 'Error'
+}
+
+function toDisplayCurrency (value) {
+  return value?.toLocaleString('en-GB', { currency: 'GBP', style: 'currency' }) ?? 'Error'
 }
 
 function grassland (values) {
   return `
     <p class="govuk-body">Based on your <strong><a href="/land-calc">${toDisplay(values['improved-grassland'])} hectares</a></strong>
     of improved grassland and <strong><a href="/land-calc">${toDisplay(values['unimproved-grassland'])} hectares</a></strong>
-    of semi-improved or unimproved grassland, you would receive <strong>£${toDisplay(values.payment)} per year</strong>,
-    including <strong>£${toDisplay(values.paymentOptional)}</strong> in extra payments.</p>`
+    of semi-improved or unimproved grassland, you would receive <strong>${toDisplayCurrency(values.payment)} per year</strong>,
+    including <strong>${toDisplayCurrency(values.paymentOptional)}</strong> in extra payments.</p>`
 }
 
 function arable (values) {
   return `
     <p class="govuk-body">Based on your <strong><a href="/land-calc">${toDisplay(values.arable)} hectares</a></strong>
-    of arable land, you would recieve <strong>£${toDisplay(values.payment)} per year</strong>, including
-    <strong>£${toDisplay(values.paymentOptional)}</strong> in extra payments.</p>`
+    of arable land, you would recieve <strong>${toDisplayCurrency(values.payment)} per year</strong>, including
+    <strong>${toDisplayCurrency(values.paymentOptional)}</strong> in extra payments.</p>`
 }
 
 function boundary (values) {
   return `
     <p class="govuk-body">Based on your <strong><a href="/land-calc">${toDisplay(values.hedgerows)} hectares</a></strong>
     of hedgerows and <strong><a href="/land-calc">${toDisplay(values['waterbody-buffers'])} hectares</a></strong> of waterbody buffering,
-    you would recieve <strong>£${toDisplay(values.payment)} per year</strong>, including
-    <strong>£${toDisplay(values.paymentOptional)}</strong> in extra payments.</p>`
+    you would recieve <strong>${toDisplayCurrency(values.payment)} per year</strong>, including
+    <strong>${toDisplayCurrency(values.paymentOptional)}</strong> in extra payments.</p>`
 }
 
 function woodland (values) {
   return `
     <p class="govuk-body">Based on your <strong><a href="/land-calc">${toDisplay(values.woodland)} hectares</a></strong>
-    of farm woodland, you would recieve <strong>£${toDisplay(values.payment)} per year</strong>, including
-    <strong>£${toDisplay(values.paymentOptional)}</strong> in extra payments.</p>`
+    of farm woodland, you would recieve <strong>${toDisplayCurrency(values.payment)} per year</strong>, including
+    <strong>${toDisplayCurrency(values.paymentOptional)}</strong> in extra payments.</p>`
 }
 
 module.exports = {
@@ -47,9 +51,9 @@ module.exports = {
   },
   getTotalFunding: (sfiTotal, sfiMonthly, bpsTotal, total) => {
     return `
-      <p class="govuk-body">For SFI you'll get <strong>£${toDisplay(sfiTotal)}</strong> in 12 monthly payments of <strong>£${toDisplay(sfiMonthly)}</strong>.</p>
-      <p class="govuk-body">Your BPS payment will be <strong>£${toDisplay(bpsTotal)}</strong>.</p>
-      <p class="govuk-body">Total: <strong>£${toDisplay(total)}</strong>.</p>
+      <p class="govuk-body">For SFI you'll get <strong>${toDisplayCurrency(sfiTotal)}</strong> in 12 monthly payments of <strong>${toDisplayCurrency(sfiMonthly)}</strong>.</p>
+      <p class="govuk-body">Your BPS payment will be <strong>${toDisplayCurrency(bpsTotal)}</strong>.</p>
+      <p class="govuk-body">Total: <strong>${toDisplayCurrency(total)}</strong>.</p>
       <p class="govuk-body">Find out how <a href="">BPS payments are reducing</a>.</p>
       <details class="govuk-details" data-module="govuk-details">
         <summary class="govuk-details__summary">
--- app/routes/ready-reckoner-v2/sfi-summary.js
+++ app/routes/ready-reckoner-v2/sfi-summary.js
@@ -2,7 +2,10 @@ const content = require('./content-scratch')
 const session = require('./session-handler')
 
 function tableRowContent (col1Text, col2Text, linkAddress) {
-  return [{ text: col1Text }, { text: col2Text }, { html: `<a href="${linkAddress}">Change</a>`, format: 'numeric' }]
+  return [
+    { text: col1Text },
+    { text: `${col2Text.toLocaleString('en-GB', { currency: 'GBP', style: 'currency' })}` },
+    { html: `<a href="${linkAddress}">Change</a>`, format: 'numeric' }]
 }
 
 const pageDetails = {
@@ -11,130 +14,18 @@ const pageDetails = {
   template: 'sfi-summary'
 }
 
-const standardAmounts = {
-  'improved-grassland': 0,
-  'improved-grassland-soils': 0,
-  'unimproved-grassland': 0,
-  arable: 0,
-  'arable-soils': 0,
-  hedgerows: 0,
-  'waterbody-buffers': 0,
-  woodland: 0
-}
-
-// const actionAmounts = {
-//   'improved-grassland0': {
-//     amount: 98,
-//     payment: 700
-//   },
-//   'improved-grassland-soils0': {
-//     amount: 87,
-//     payment: 600
-//   },
-//   'improved-grassland-soils1': {
-//     amount: 76,
-//     payment: 500
-//   },
-//   arable0: {
-//     amount: 65,
-//     payment: 400
-//   },
-//   'arable-soils0': {
-//     amount: 54,
-//     payment: 300
-//   },
-//   woodland0: {
-//     amount: 43,
-//     payment: 200
-//   },
-//   'waterbody-buffers0': {
-//     amount: 32,
-//     payment: 100
-//   }
-// }
-
-const paymentAmounts = {
-  'improved-grassland': {
-    base: 150,
-    optional: {
-      'improved-grassland0': 100
-    }
-  },
-  'improved-grassland-soils': {
-    base: 250,
-    optional: {
-      'improved-grassland-soils0': 200,
-      'improved-grassland-soils1': 225
-    }
-  },
-  'unimproved-grassland': {
-    base: 350,
-    optional: {}
-  },
-  arable: {
-    base: 450,
-    optional: {
-      arable0: 400
-    }
-  },
-  'arable-soils': {
-    base: 550,
-    optional: {
-      'arable-soils0': 500
-    }
-  },
-  hedgerows: {
-    base: 650,
-    optional: {}
-  },
-  'waterbody-buffers': {
-    base: 750,
-    optional: {
-      'waterbody-buffers0': 700
-    }
-  },
-  woodland: {
-    base: 850,
-    optional: {
-      woodland0: 800
-    }
-  }
-}
-
-// const categoryAmounts = {
-//   grassland: {
-//     amountImproved: null,
-//     amountUnimproved: null,
-//     payment: 321,
-//     paymentOptional: 432
-//   },
-//   arable: {
-//     amount: null,
-//     payment: 543,
-//     paymentOptional: 654
-//   },
-//   boundary: {
-//     amountHedgerows: null,
-//     amountWaterbody: null,
-//     payment: 765,
-//     paymentOptional: 876
-//   },
-//   woodland: {
-//     amount: null,
-//     payment: 987,
-//     paymentOptional: 789
-//   }
-// }
-
-function pageContent (categoryAmounts, actionValues, errorText = null) {
+function pageContent (categoryAmounts, actionValues, paymentAmounts) {
   return {
     title: 'Summary',
     hint: 'How much you will get in 2022.',
-    errorText,
     backPath: pageDetails.backPath,
     components: {
       insetText: {
-        html: content.getTotalFunding(70002.123456, 6000.9999, 8000.3292, 78000.11111)
+        html: content.getTotalFunding(
+          paymentAmounts.sfiTotal,
+          paymentAmounts.sfiMonthly,
+          paymentAmounts.bpsPayment,
+          paymentAmounts.grandTotal)
       },
       summaryTitle: 'Funding breakdown',
       summaryList: content.getFundingBreakdown().map(details => ({
@@ -177,6 +68,44 @@ function pageContent (categoryAmounts, actionValues, errorText = null) {
   }
 }
 
+function doCalculations(landValues, actionValues, bpsPayment) {
+  const standardsRates = content.standardsRates
+  const landFeatures = content.landFeatures
+  const standards = content.standards
+
+  const paymentTotals = {
+    sfiTotal: 0,
+    bpsPayment: bpsPayment * 0.8 // FIXME
+  }
+
+  Object.entries(landFeatures).forEach(([featureId, feature]) => {
+    feature.standards.forEach(standardId => {
+      paymentTotals[standardId] = {
+        base: landValues[featureId] * standardsRates[standardId].mandatory,
+        optional: {}
+      }
+
+      paymentTotals.sfiTotal += paymentTotals[standardId].base
+
+      standards[standardId].optionalActions.forEach((actionId, i) => {
+        paymentTotals[standardId].optional[actionId] = (actionValues?.[actionId] ?? 0) * standardsRates[standardId].optional[i]
+
+        // Payment rates for this is in hectares, but user input is in meters square
+        if (actionId === 'woodland0') {
+          paymentTotals[standardId].optional[actionId] /= 10000
+        }
+
+        paymentTotals.sfiTotal += paymentTotals[standardId].optional[actionId]
+      })
+    })
+  })
+
+  paymentTotals['sfiMonthly'] = paymentTotals.sfiTotal / 12
+  paymentTotals['grandTotal'] = paymentTotals.sfiTotal + paymentTotals.bpsPayment
+
+  return paymentTotals
+}
+
 module.exports = [
   {
     method: 'GET',
@@ -185,8 +114,12 @@ module.exports = [
       // FIXME: is there a nicer way of doing this?
       pageDetails.backPath = '/' + request.info.referrer.split('/').slice(-1)[0]
 
+      // Do payment calculation
       const landValues = session.getValue(request, session.keys.landValues)
       const actionValues = session.getValue(request, session.keys.actionValues)
+      const bpsPayment = session.getValue(request, session.keys.bpsPayment)
+      const paymentAmounts = doCalculations(landValues, actionValues, bpsPayment)
+
       const selectedStandards = session.getValue(request, session.keys.selectedStandards)
       const landFeatures = content.landFeatures
       const landFeatureCategories = content.landFeatureCategories
@@ -214,10 +147,7 @@ module.exports = [
         })
       })
 
-      console.log(categoryAmounts)
-      console.log(actionValues)
-
-      return h.view(pageDetails.template, pageContent(categoryAmounts, actionValues))
+      return h.view(pageDetails.template, pageContent(categoryAmounts, actionValues, paymentAmounts))
     }
   },
   {
