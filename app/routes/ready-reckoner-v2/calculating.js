c608eb2889409ab3263f0ff9566ef386bd26a58e
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-02-15 08:26:12 +0000

    refactor: improve naming, pass all required data

--- app/routes/ready-reckoner-v2/calculating.js
+++ app/routes/ready-reckoner-v2/calculating.js
@@ -1,26 +1,26 @@
 const { v4: uuid } = require('uuid')
 
+const baseStandards = require('../../services/standards-v2')
 const session = require('./session-handler')
-const standardsTemplate = require('../../services/standards-v2')
 const { updateAgreement } = require('../../messaging/senders')
 
 // NOTE: map the land types to the standards. doesn't matter which way round as
 //       the relationship is assumed to be 1-to-1
-function hydrateStandards (landValues, actionValues, selectedStandards) {
+function hydrateStandards (baseStandards, selectedStandards, landValues, actionValues) {
   // Could filter landValues to only those with a value greater than 0 but by
   // not doing this, they can run through the calculation and everything will
   // work, it'll be easier to reason about and if they were to be required
   // within the calculation at some point they are already there
-  const actionValueSet = Object.keys(actionValues)
+  const actionValueList = Object.keys(actionValues)
   return Object.entries(landValues).reduce((acc, cur) => {
     const [k, v] = cur
-    standardsTemplate.filter(s => s.landType === k).forEach(s => {
+    baseStandards.filter(s => s.landType === k).forEach(s => {
       s.inputValue = Number(v)
       // flag to determine which standard(s) are included in total payment calc
       s.selected = selectedStandards.includes(s.id)
       acc[s.id] = s
       // actions are implicitly selected if they contain a value, no need to flag them as such
-      s.optionalActions.filter(a => actionValueSet.includes(a.id)).forEach(a => {
+      s.optionalActions.filter(a => actionValueList.includes(a.id)).forEach(a => {
         a.inputValue = actionValues[a.id]
       })
     })
@@ -35,10 +35,8 @@ module.exports = [{
     const landValues = session.getValue(request, session.keys.landValues)
     const actionValues = session.getValue(request, session.keys.actionValues)
     const selectedStandards = session.getValue(request, session.keys.selectedStandards)
-    const standards = hydrateStandards(landValues, actionValues, selectedStandards)
+    const standards = hydrateStandards(baseStandards, selectedStandards, landValues, actionValues)
 
-    // TODO: msg format doesn't include calculations. calc MS has been updated
-    //       to support msgs with and without calculations
     const standardsSet = {
       id: 'standardsSet',
       standards
