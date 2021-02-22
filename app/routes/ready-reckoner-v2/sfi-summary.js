1403488a8ef497739d045793b46e795bf28f4a84
Paul Andrews <paul.andrews@defra.gov.uk>
2021-02-22 11:57:29 +0000

    More logging

--- app/routes/ready-reckoner-v2/sfi-summary.js
+++ app/routes/ready-reckoner-v2/sfi-summary.js
@@ -137,7 +137,9 @@ module.exports = [
 
       const correlationId = session.getValue(request, session.keys.correlationId)
       const url = `${agreementServiceBaseUrl}/value?correlationId=${correlationId}`
+      console.log('****')
       console.log(`HITTING this: ${url}`)
+      console.log('****')
       const { payload } = await Wreck.get(url, { json: true })
       log('msg response', payload)
 
